4bdc7bfc3f2b7b13a0ad0a03676c00ebde8e3133
yuichiban <bancjapan@gmail.com>
2022-07-18 17:18:35 +0900

    修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -98,6 +98,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is invalid")
       end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
